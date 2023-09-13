5a17752db2284a313e694b75979e6f038271ce0d
pigchip <guzmancruzandresmiguel@gmail.com>
2023-09-13 15:29:55 -0600

    Added clerk authentication

--- ai-saas/app/(dashboard)/(routes)/dashboard/page.tsx
+++ ai-saas/app/(dashboard)/(routes)/dashboard/page.tsx
@@ -1,7 +1,12 @@
+import { UserButton } from "@clerk/nextjs";
+
 const DashboardPage = () => {
   return (
-    <p>Dashboard Page (Protected)</p>
-  )
+    <div>
+      <p>Dashboard Page (Protected)</p>
+      <UserButton afterSignOutUrl="/"></UserButton>
+    </div>
+    )
 }
 
 export default DashboardPage;
