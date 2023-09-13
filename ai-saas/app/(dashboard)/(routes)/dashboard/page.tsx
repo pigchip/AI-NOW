52907d7d189e7f611123133143033c1e2e601ee9
pigchip <guzmancruzandresmiguel@gmail.com>
2023-09-13 17:51:44 -0600

    Added dashboard panel.

--- ai-saas/app/(dashboard)/(routes)/dashboard/page.tsx
+++ ai-saas/app/(dashboard)/(routes)/dashboard/page.tsx
@@ -1,7 +1,79 @@
+"use client";
+
+import { Card } from "@/components/ui/card";
+import { ArrowRight, Code, ImageIcon, MessageSquare, Music, Video } from "lucide-react";
+import { cn } from "@/lib/utils"
+import { useRouter } from "next/navigation";
+
+const tools = [
+  {
+    label: "Conversation",
+    icon: MessageSquare,
+    color: "text-violet-500",
+    bgColor: "bg-violet-500/10",
+    href: "/conversation"
+  },
+  {
+    label: "Music Generation",
+    icon: Music,
+    color: "text-emerald-500",
+    bgColor: "bg-emerald-500/10",
+    href: "/music"
+  },
+  {
+    label: "Image Generation",
+    icon: ImageIcon,
+    color: "text-pink-700",
+    bgColor: "bg-pink-700/10",
+    href: "/image"
+  },
+  {
+    label: "Video Generation",
+    icon: Video,
+    color: "text-orange-500",
+    bgColor: "bg-orange-500/10",
+    href: "/video"
+  },
+  {
+    label: "Code Generation",
+    icon: Code,
+    color: "text-green-700",
+    bgColor: "bg-green-700/10",
+    href: "/code"
+  },
+]
+
 const DashboardPage = () => {
+  const router = useRouter();
   return (
     <div>
-      <p>Dashboard Page (Protected)</p>
+      <div className="mb-8 space-y-4">
+        <h2 className="text-2xl md:text-4xl font-bold text-center">
+          "AI-NOW: Unleashing Digital Potential"
+        </h2>
+        <p className="text-muted-foreground font-light text-sm md:text-lg text-center">
+        "Delve into the AI-NOW, where the convergence of innovative technology and artificial intelligence transforms the way you interact, create, and envision the digital realm."
+        </p>
+      </div>
+      <div className="px-4 md:px-20 lg:px-32 space-y-4">
+        {tools.map((tool) => (
+          <Card 
+          onClick={() => router.push(tool.href)}
+          key={tool.href}
+          className="p-4 border-black/5 flex items-center justify-between hover:shadow-md transition cursor-pointer"
+          >
+            <div className="flex items-center gap-x-4">
+              <div className={cn("p-2 w-fit rounded-md", tool.bgColor)}>
+                <tool.icon className={cn("w-8 h-8", tool.color)}/>
+              </div>
+              <div className="font-semibold">
+                {tool.label}
+              </div>
+            </div>
+            <ArrowRight className="w-5 h-5"/>
+          </Card>
+        ))}
+      </div>
     </div>
     )
 }
--- ai-saas/components/sidebar.tsx
+++ ai-saas/components/sidebar.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { Montserrat } from "next/font/google";
+import { usePathname } from "next/navigation";
 import Image from "next/image";
 import Link from "next/link";
 import { cn } from "@/lib/utils";
@@ -64,6 +65,7 @@ const routes = [
 ];
 
 const Sidebar = () => {
+    const pathname = usePathname();
     return (
         <div className="space-y-4 py-4 flex flex-col h-full bg-[#000000] text-white">
             <div className="px-3 py-2 flex-1">
@@ -84,7 +86,9 @@ const Sidebar = () => {
                         <Link
                             href={route.href}
                             key={route.href}
-                            className="text-sm group flex p-3 w-full justify-start font-medium cursor-pointer hover:text-white hover:bg-white/10 rounded-lg transition"    
+                            className={cn("text-sm group flex p-3 w-full justify-start font-medium cursor-pointer hover:text-white hover:bg-white/10 rounded-lg transition",
+                            pathname === route.href ? "text-white bg-white/10" : "text-zinc-400"
+                            )}   
                         >
                             <div className="flex items-center flex-1">
                                 <route.icon className={cn("h-5 w-5 mr-3", route.
