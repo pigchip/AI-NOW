af072f6b93548a3e9ce3a6686acef6fc5f06fd8a
pigchip <guzmancruzandresmiguel@gmail.com>
2023-09-13 17:14:14 -0600

    Added sidebar for pc and mobile.

---
+++ ai-saas/components/navbar.tsx
@@ -0,0 +1,17 @@
+import { Button } from "@/components/ui/button";
+import { UserButton } from "@clerk/nextjs";
+import { Menu } from "lucide-react";
+import MobileSidebar from "@/components/mobile-sidebar";
+
+const Navbar = () => {
+    return(
+        <div className="flex items-center p-4">
+            <MobileSidebar />
+            <div className="flex w-full justify-end">
+                <UserButton afterSignOutUrl="/"/>    
+            </div>             
+        </div>
+    );
+}
+
+export default Navbar;
